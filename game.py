4c15723bf8ace3bcfefcd598eeca75cf9eae3973
Ansh Kumar <115077985+ansh3108@users.noreply.github.com>
2024-07-06 10:37:22 +0530

    Create game.py

---
+++ game.py
@@ -0,0 +1,145 @@
+import pygame
+import sys
+import random
+import math
+
+pygame.init()
+pygame.display.set_caption("Snake Game")
+pygame.font.init()
+random.seed()
+
+SPEED = 0.36
+SNAKE_SIZE = 9
+APPLE_SIZE = SNAKE_SIZE
+SEPARATION = 10
+SCREEN_HEIGHT = 600
+SCREEN_WIDTH = 800
+FPS = 25
+KEY = {"UP": 1, "DOWN": 2, "LEFT": 3, "RIGHT": 4}
+
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT), pygame.HWSURFACE)
+
+score_font = pygame.font.Font(None, 38)
+score_numb_font = pygame.font.Font(None, 28)
+game_over_font = pygame.font.Font(None, 46)
+play_again_font = score_numb_font
+score_msg = score_font.render("Score : ", 1, pygame.Color("green"))
+score_msg_size = score_font.size("Score")
+background_color = pygame.Color(0, 0, 0)
+black = pygame.Color(0, 0, 0)
+
+gameClock = pygame.time.Clock()
+
+def checkCollision(posA, As, posB, Bs):
+    if posA.x < posB.x + Bs and posA.x + As > posB.x and posA.y < posB.y + Bs and posA.y + As > posB.y:
+        return True
+    return False
+
+def checkLimits(snake):
+    if snake.x > SCREEN_WIDTH:
+        snake.x = SNAKE_SIZE
+    if snake.x < 0:
+        snake.x = SCREEN_WIDTH - SNAKE_SIZE
+    if snake.y > SCREEN_HEIGHT:
+        snake.y = SNAKE_SIZE
+    if snake.y < 0:
+        snake.y = SCREEN_HEIGHT - SNAKE_SIZE
+
+class Apple:
+    def __init__(self, x, y, state):
+        self.x = x
+        self.y = y
+        self.state = state
+        self.color = pygame.color.Color("orange")
+
+    def draw(self, screen):
+        pygame.draw.rect(screen, self.color, (self.x, self.y, APPLE_SIZE, APPLE_SIZE), 0)
+
+class segment:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.direction = KEY["UP"]
+        self.color = "white"
+
+class snake:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.direction = KEY["UP"]
+        self.stack = []
+        self.stack.append(self)
+        blackBox = segment(self.x, self.y + SEPARATION)
+        blackBox.direction = KEY["UP"]
+        blackBox.color = "NULL"
+        self.stack.append(blackBox)
+
+    def move(self):
+        last_element = len(self.stack) - 1
+        while last_element != 0:
+            self.stack[last_element].direction = self.stack[last_element - 1].direction
+            self.stack[last_element].x = self.stack[last_element - 1].x
+            self.stack[last_element].y = self.stack[last_element - 1].y
+            last_element -= 1
+        if len(self.stack) < 2:
+            last_segment = self
+        else:
+            last_segment = self.stack.pop(last_element)
+        last_segment.direction = self.stack[0].direction
+        if self.stack[0].direction == KEY["UP"]:
+            last_segment.y = self.stack[0].y - (SPEED * FPS)
+        elif self.stack[0].direction == KEY["DOWN"]:
+            last_segment.y = self.stack[0].y + (SPEED * FPS)
+        elif self.stack[0].direction == KEY["LEFT"]:
+            last_segment.x = self.stack[0].x - (SPEED * FPS)
+        elif self.stack[0].direction == KEY["RIGHT"]:
+            last_segment.x = self.stack[0].x + (SPEED * FPS)
+        self.stack.insert(0, last_segment)
+
+    def getHead(self):
+        return self.stack[0]
+
+    def grow(self):
+        last_element = len(self.stack) - 1
+        self.stack[last_element].direction = self.stack[last_element].direction
+        if self.stack[last_element].direction == KEY["UP"]:
+            newSegment = segment(self.stack[last_element].x, self.stack[last_element].y - SNAKE_SIZE)
+            blackBox = segment(newSegment.x, newSegment.y - SEPARATION)
+        elif self.stack[last_element].direction == KEY["DOWN"]:
+            newSegment = segment(self.stack[last_element].x, self.stack[last_element].y + SNAKE_SIZE)
+            blackBox = segment(newSegment.x, newSegment.y + SEPARATION)
+        elif self.stack[last_element].direction == KEY["LEFT"]:
+            newSegment = segment(self.stack[last_element].x - SNAKE_SIZE, self.stack[last_element].y)
+            blackBox = segment(newSegment.x - SEPARATION, newSegment.y)
+        elif self.stack[last_element].direction == KEY["RIGHT"]:
+            newSegment = segment(self.stack[last_element].x + SNAKE_SIZE, self.stack[last_element].y)
+            blackBox = segment(newSegment.x + SEPARATION, newSegment.y)
+        blackBox.color = "NULL"
+        self.stack.append(newSegment)
+        self.stack.append(blackBox)
+
+    def setDirection(self, direction):
+        if (self.direction == KEY["RIGHT"] and direction == KEY["LEFT"]) or (self.direction == KEY["LEFT"] and direction == KEY["RIGHT"]):
+            pass
+        elif (self.direction == KEY["UP"] and direction == KEY["DOWN"]) or (self.direction == KEY["DOWN"] and direction == KEY["UP"]):
+            pass
+        else:
+            self.direction = direction
+
+    def checkCrashing(self):
+        counter = 1
+        while counter < len(self.stack) - 1:
+            if checkCollision(self.stack[0], SNAKE_SIZE, self.stack[counter], SNAKE_SIZE) and self.stack[counter].color != "NULL":
+                return True
+            counter += 1
+        return False
+
+    def draw(self, screen):
+        pygame.draw.rect(screen, pygame.color.Color("green"), (self.stack[0].x, self.stack[0].y, SNAKE_SIZE, SNAKE_SIZE), 0)
+        counter = 1
+        while counter < len(self.stack):
+            if self.stack[counter].color == "NULL":
+                counter += 1
+                continue
+            pygame.draw.rect(screen, pygame.color.Color("yellow"), (self.stack[counter].x, self.stack[counter].y, SNAKE_SIZE, SNAKE_SIZE), 0)
+            counter += 1
